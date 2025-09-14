04270812ff387d99f6567c11f4c6fe48079300ec
jeffry <thisisjeffry77@gmail.com>
2025-09-14 19:29:44 +0300

    feat: URL paths

--- scripts/headers/headers.js
+++ scripts/headers/headers.js
@@ -6,14 +6,14 @@ export function renderHeaders() {
         </div>
         <div class="header-right">
           <nav class="nav-menu">
-            <a class="nav-item home-page-nav-item" title="Go back to the home page" href="homePage.html">
+            <a class="nav-item home-page-nav-item" title="Go back to the home page" href="../index.html">
               Home
             </a>
-            <a class="nav-item about-me-nav-item" href="aboutMe.html">
+            <a class="nav-item about-me-nav-item" href="/aboutMe/">
               About me
             </a>
             <div class="nav-seperator"></div>
-            <a class="nav-item my-services-nav-item" href="services.html">
+            <a class="nav-item my-services-nav-item" href="/services/">
               My services
             </a>
             <a class="nav-item contact-me-nav-item js-contact-me-nav-item">
@@ -33,7 +33,7 @@ export function renderHeaders() {
           <p class="header-left-text">Coach Sona Bakalian</p>
         </div>
         <div class="hamburger-menu-icon-container">
-        <img class="hamburger-menu-icon js-hamburger-menu-icon" src="images/icons/align-left (1).svg">
+        <img class="hamburger-menu-icon js-hamburger-menu-icon" src="../images/icons/align-left (1).svg">
       </div>
       <div class="hamburger-menu-page js-hamburger-menu-page">
         <div class="hamburger-menu-page-header">
@@ -41,9 +41,9 @@ export function renderHeaders() {
         <img class="close-icon js-close-icon" src="images/icons/cross.svg">
       </div>
       <div class="hamburger-menu-page-options">
-        <a class="hamburger-menu-page-option home-page-nav-item" href="homePage.html">Home</a>
-        <a class="hamburger-menu-page-option about-me-nav-item" href="aboutMe.html">About me</a>
-        <a class="hamburger-menu-page-option my-services-nav-item" href="services.html">My services</a>
+        <a class="hamburger-menu-page-option home-page-nav-item" href="/index.html">Home</a>
+        <a class="hamburger-menu-page-option about-me-nav-item" href="/aboutMe/">About me</a>
+        <a class="hamburger-menu-page-option my-services-nav-item" href="/services/">My services</a>
         <a class="hamburger-menu-page-option contact-me-nav-item hamburger-menu-page-option-contact-me-nav-item js-contact-me-nav-item">Contact me</a>
       </div>
     </div>
